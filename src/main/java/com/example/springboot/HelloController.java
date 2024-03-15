236e5e323e53847336d173c2dab97f7ad4d79349
Vasanth Desikan <vdesikanvmware@gmail.com>
2024-03-14 22:00:03 -0400

    1710468001

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + 1710460801!";
+		return "Greetings from Spring Boot + Tanzu + 1710468001!";
 	}
 
 }
